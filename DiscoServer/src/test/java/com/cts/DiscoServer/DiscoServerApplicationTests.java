ec526eec5191a7b27bcbae306fac733d6d553c99
shekarchandru <52452150+shekarchandru@users.noreply.github.com>
2024-03-23 16:42:12 +0530

    Add files via upload

---
+++ DiscoServer/src/test/java/com/cts/DiscoServer/DiscoServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cts.DiscoServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
